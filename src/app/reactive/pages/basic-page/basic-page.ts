027e57ab4bdcdc224e63caeec4b3b85f05627a12
Daniel Hernandez <danielakire07@gmail.com>
2026-04-14 17:56:40 -0500

    feat: validaciones en campos requeridos mediante controls

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,12 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -8,17 +14,16 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angul
   templateUrl: './basic-page.html',
 })
 export class BasicPage {
-
-  private fb = inject (FormBuilder);
-  myForm =this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+  private fb = inject(FormBuilder);
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(10)]],
   });
 
   //myForm = new FormGroup({
-    //name: new FormControl(''),
-    //price: new FormControl(0),
-    //inStorage: new FormControl(0),
+  //name: new FormControl(''),
+  //price: new FormControl(0),
+  //inStorage: new FormControl(0),
   //});
 }
